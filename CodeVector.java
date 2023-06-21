6f11d9b8e2f28e58f81611e38ec93e5d03235590
Lciarallo <85714365+Lciarallo@users.noreply.github.com>
2023-06-21 19:27:36 -0300

    Create CodeVector.java

---
+++ CodeVector.java
@@ -0,0 +1,12 @@
+
+import java.util.Vector;
+
+public class VectorExample {
+    public static void main(String[] args) {
+        Vector<Integer> numbers = new Vector<>();
+numbers.add(10);
+numbers.add(12);
+System.out.println(numbers.get(0));
+
+    }
+}
